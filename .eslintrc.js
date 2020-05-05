b236dcb825fc002f042b65cf9a1c797235a30888
rajan <rajan@freshbits.in>
2020-05-05 18:17:35 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/require-render-return': 'off',
+    'vue/return-in-computed-property': 'off'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/prettier'
+  ]
+};
